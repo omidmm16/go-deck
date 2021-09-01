a099acf46b362d15ccdbbf9f8de1cd0947c227a7
Omid Mamandi <omidmm16@gmail.com>
2021-09-01 13:03:30 +0430

    shuffle

--- main.go
+++ main.go
@@ -2,17 +2,8 @@ package main
 
 func main() {
 
-	// cards := newDeck()
-	// hand, remainingCards := deal(cards, 3)
-	// fmt.Println("Cards in hand")
-	// hand.print()
-	// fmt.Println("Remain cards")
-	// remainingCards.print()
-
-	// cards := newDeck()
-
-	// fmt.Println(cards.saveToFile("my_cards"))
-	cards := newDeckFromFile("my_card")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 
 }
